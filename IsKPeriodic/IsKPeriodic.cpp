96bc3c0380ba079a5b9ebcf7f4556e29dce81ba7
123 <123@DESKTOP-NFGAGNS>
2023-04-14 20:08:45 +0300

    Добавьте файлы проекта.

---
+++ IsKPeriodic/IsKPeriodic.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+bool IsKPeriodic(string str, int k)
+{
+    if (k >= str.size())
+        return false;
+    
+    for (int i = 0; i < str.size() - k; i++)
+    {
+        if (str[i] != str[i + k])
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    cout << IsKPeriodic("abcabcabcabc",3);
+}
+
